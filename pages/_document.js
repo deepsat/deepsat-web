d45b0827ec5b149a12a7da5bd9b6aa4887d4dde0
Piotr Grynfelder <dev.pitek@gmail.com>
2020-11-05 22:19:02 +0100

    move icons and stuff to public from pulbic/favicon

--- pages/_document.js
+++ pages/_document.js
@@ -15,31 +15,31 @@ class MyDocument extends Document {
             <link
               rel="apple-touch-icon"
               sizes="180x180"
-              href="/favicon/apple-touch-icon.png?v=1"
+              href="/apple-touch-icon.png?v=1"
             />
             <link
               rel="icon"
               type="image/png"
               sizes="32x32"
-              href="/favicon/favicon-32x32.png?v=1"
+              href="/favicon-32x32.png?v=1"
             />
             <link
               rel="icon"
               type="image/png"
               sizes="16x16"
-              href="/favicon/favicon-16x16.png?v=1"
+              href="/favicon-16x16.png?v=1"
             />
-            <link rel="manifest" href="/favicon/site.webmanifest?v=1" />
+            <link rel="manifest" href="/site.webmanifest?v=1" />
             <link
               rel="mask-icon"
-              href="/favicon/safari-pinned-tab.svg?v=1"
+              href="/safari-pinned-tab.svg?v=1"
               color="#000000"
             />
             <link rel="shortcut icon" href="/favicon/favicon.ico?v=1" />
             <meta name="msapplication-TileColor" content="#000000" />
             <meta
               name="msapplication-config"
-              content="/favicon/browserconfig.xml?v=1"
+              content="/browserconfig.xml?v=1"
             />
             <meta name="theme-color" content="#000000" />
           </Head>
