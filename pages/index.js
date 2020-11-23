0f5ce89c1a7494ea7f919b8f37264298f1295079
Piotr Grynfelder <dev.pitek@gmail.com>
2020-11-23 16:40:25 +0100

    modify bottom whitespace

--- pages/index.js
+++ pages/index.js
@@ -144,7 +144,7 @@ export default function Index({ statics, sections, feed, team, menu }) {
       <Section heading={sections.contact.title} id="contact">
         <RTF>{sections.contact.content}</RTF>
       </Section>
-      <Box height="100vh" />
+      <Box height="50vh" />
     </Layout>
   );
 }
