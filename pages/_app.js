c5a47c84701bf6a4fff4b2fda71addbf8028af88
Piotr Grynfelder <dev.pitek@gmail.com>
2021-06-28 16:36:44 +0200

    move to @fontsource packages

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,9 @@
-import "fontsource-montserrat";
-import "fontsource-montserrat/500.css";
-import "fontsource-montserrat/600.css";
-import "fontsource-montserrat/700.css";
-import "fontsource-open-sans";
-import "fontsource-open-sans/700.css";
+import "@fontsource/montserrat";
+import "@fontsource/montserrat/500.css";
+import "@fontsource/montserrat/600.css";
+import "@fontsource/montserrat/700.css";
+import "@fontsource/open-sans";
+import "@fontsource/open-sans/700.css";
 import { ChakraProvider, CSSReset } from "@chakra-ui/react";
 import theme from "../theme";
 import "focus-visible/dist/focus-visible";
